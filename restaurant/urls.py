80e6b6c0b51b9aeec1c8eb62e02a2510e06c2499
sirchimex <chimexchidile@gmail.com>
2023-10-17 16:25:26 +0100

    Added Serializers.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='home')
+    # path('', views.index, name='home'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import generics 
+from .models import Menu
+from .serializers import MenuItemSerializer
 
-# Create your views here.
-def  index(request):
-    return render(request, 'index.html', {})
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
